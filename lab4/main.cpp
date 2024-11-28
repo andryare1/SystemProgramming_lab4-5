187e7e0ad2a643d4604bad0913103a97daf16ead
andryare <andreyyarema.2005@gmail.com>
2024-11-28 13:55:29 +0300

    Initial Commit

---
+++ lab4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab4
+//
+//  Created by Андрей Ярема on 28.11.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
