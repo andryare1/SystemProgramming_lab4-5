da91cf6d76e3266b22e0662853ecf6885966983e
andryare1 <130035154+andryare1@users.noreply.github.com>
2024-12-04 01:31:09 +0300

    Update Circle.hpp

--- lab4/Circle.hpp
+++ lab4/Circle.hpp
@@ -7,7 +7,34 @@
 
 #ifndef Circle_hpp
 #define Circle_hpp
-
 #include <stdio.h>
+
+class Circle{
+    
+private:
+    double x;
+    double y;
+    double radius;
+    static int count;
+    const int id;
+
+public:
+    Circle();
+    Circle(double x, double y, double radius);
+    Circle(const Circle& otherCircle);
+    ~Circle();
+    
+    Circle& operator=(const Circle& otherCircle);
+    Circle operator+(const Circle& otherCircle) const;
+    Circle operator*(const Circle& otherCircle) const;
+    
+    void display() const;
+    double getX() const;
+    double getY() const;
+    double getRadius() const;
+    static int getCount();
+    int getId() const;
+
+};
 
 #endif /* Circle_hpp */
