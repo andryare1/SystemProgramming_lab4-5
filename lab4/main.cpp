32ca76b7b29347942fe8583c38adea1b02136283
andryare1 <130035154+andryare1@users.noreply.github.com>
2024-12-04 01:31:46 +0300

    Update main.cpp

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -6,9 +6,34 @@
 //
 
 #include <iostream>
+#include "Circle.hpp"
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+   
+    cout << "Начальное количество объектов: " << Circle::getCount() << endl;
+    Circle circle1(0, 0, 33);
+    Circle circle2(0, 4, 6);
+
+    circle1.display();
+    cout << "ID объекта circle1: " << circle1.getId() << endl;
+    circle2.display();
+    cout << "ID объекта circle2: " << circle2.getId() << endl;
+    
+    cout << "Количество объектов после создания: " << Circle::getCount() << endl;
+    
+
+    Circle intersection = circle1 * circle2;
+    cout << "Пересечение окружности 1 и окружности 2: ";
+    intersection.display();
+
+    Circle unionCircle = circle1 + circle2;
+    cout << "Объединение окружности 1 и окружности 2: ";
+    unionCircle.display();
+    
+    cout << "Количество объектов после создания новых окружностей: " << Circle::getCount() << endl;
+    int q = 0;
+    cin >> q;
     return 0;
 }
