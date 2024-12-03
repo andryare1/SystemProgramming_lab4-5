a955c815b4739d8c96961e119a66ea309b5911fc
andryare <andreyyarema.2005@gmail.com>
2024-12-04 00:18:47 +0300

    lab4-5

---
+++ lab4/Circle.cpp
@@ -0,0 +1,8 @@
+//
+//  Circle.cpp
+//  lab4
+//
+//  Created by Андрей Ярема on 28.11.2024.
+//
+
+#include "Circle.hpp"
---
+++ lab4/Circle.hpp
@@ -0,0 +1,13 @@
+//
+//  Circle.hpp
+//  lab4
+//
+//  Created by Андрей Ярема on 28.11.2024.
+//
+
+#ifndef Circle_hpp
+#define Circle_hpp
+
+#include <stdio.h>
+
+#endif /* Circle_hpp */
